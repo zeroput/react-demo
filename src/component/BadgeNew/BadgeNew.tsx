493046665c2d458cb8b7cd7e6d3c7385a0e071c4
XiaShuang <zachary_xia@163.com>
2024-09-10 21:12:05 +0800

    use {children} to display the data between the label <Label>xxx</Label>

--- src/component/BadgeNew/BadgeNew.tsx
+++ src/component/BadgeNew/BadgeNew.tsx
@@ -1,56 +1,32 @@
 import './BadgeNew.css'
 
-// return each element by function 
-function Avatar({name, imgUrl}) {
-  return <img src={imgUrl} alt={name} />;
-}
-
-function Name( {name}) {
-  return <h4>{name}</h4>;
-}
-
-function Handle({handle}) {
-  return <p>@{handle}</p>;
-}
-
-function Badge({user, style, addFriend}) {
+// in this function, I found that when I use {children}, it will succeed, but if I use {child}, it will compile fail
+function Badge({ name, img, handle, children }) {
   return (
-    <div style={style}>
-      <Avatar name={user.name} 
-      imgUrl={user.img} />
-      <div>
-        <Name name={user.name} />
-        <Handle handle={user.handle} />
-        <button onClick={addFriend}>Add Friend</button>
-      </div>
+    <div className="badge">
+      <img alt={`Avatar for ${name}`} src={img} />
+      <header>
+        <h4>{name}</h4>
+        <h6>@{handle}</h6>
+      </header>
+      {children}
     </div>
   );
 }
 
-
-
 export default function App() {
+  const handleAddFriend = () => alert("Added, Successfully");
+
   return (
     <Badge
-      user={{
-        name: "Lynn Fisher",
-        img: "https://avatars.githubusercontent.com/u/871315",
-        handle: "hello world"
-      }}
-      style={{
-        width: 300,
-        margin: "0 auto",
-        border: "1px solid var(--beige-10)",
-        borderRadius: 8,
-        backgroundColor: "grey",
-        padding: 24,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        gap: "16px",
-        textAlign: "center"
-      }}
-      addFriend={() => alert("Added!")}
-    />
+      name="Alex Brown"
+      handle="alexbrown40"
+      img="https://avatars.githubusercontent.com/u/67877015"
+    >
+      <p>
+        Alex enjoys running, surfing, and binge watching the Twilight series.
+      </p>
+      <button onClick={handleAddFriend}>Add Friend</button>
+    </Badge>
   );
 }
