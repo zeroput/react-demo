a63c819af0ba3cb9af45dcf2582d53cf1b1e2c9b
XiaShuang <zachary_xia@163.com>
2024-09-10 16:23:55 +0800

    use inner html and map function in react

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,15 @@ function App() {
   var classBind = 'test'
   let my_style = {color : 'blue'}
 
+  let innerHtmlStr = `
+  <section>
+  <h1>12. this is a inner html for a test by geminix</h1>
+  </section>
+  `
   
+  // use iter to iter the element, like v-for in vue.js
+  const my_arr_test = [1,2,3,1000]
+
   return (
     <>
       <div>
@@ -46,13 +54,25 @@ function App() {
         <h2 onClick={myFn}>7. Click me</h2>
         <button onClick={ (e)=>fnWithParams(3333, e) }>Run Alert</button>
         <br/>
-        <button  onClick={ ()=>myFnGeric(5555) }>Run Generaic</button>
+        <button  onClick={ ()=>myFnGeric(5555) }>Run Generic Function</button>
         <h2 id={idBind}>
           8. id is {idBind}
         </h2>
         <h2 className={classBind}>9. Bind the css class </h2>
         <h2 style={my_style}>10. bind the style by using variable</h2>
         <h2 style={{color : 'grey'}}>11. bind the style use obj directly</h2>
+        <div dangerouslySetInnerHTML={{__html : innerHtmlStr}}></div>
+        {/* this will iter the number array by using arr.map() function */}
+        <div id='test the iter'>
+          {
+            my_arr_test.map(ele => {
+              return  <div id={ele.toString()} key={ele}>
+                      <h5>{ele}</h5>
+                    </div>
+            })
+          }
+        </div>
+        
       </div>
     </>
   )
