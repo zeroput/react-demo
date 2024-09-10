3d1ebeeea7b3722012199b4c41819b90089ae5f3
XiaShuang <zachary_xia@163.com>
2024-09-10 21:05:43 +0800

    refacor the Badge component by using function to return the fragment

--- src/component/BadgeNew/BadgeNew.tsx
+++ src/component/BadgeNew/BadgeNew.tsx
@@ -1,22 +1,56 @@
 import './BadgeNew.css'
-function Badge({ name, handle, img }) {
+
+// return each element by function 
+function Avatar({name, imgUrl}) {
+  return <img src={imgUrl} alt={name} />;
+}
+
+function Name( {name}) {
+  return <h4>{name}</h4>;
+}
+
+function Handle({handle}) {
+  return <p>@{handle}</p>;
+}
+
+function Badge({user, style, addFriend}) {
   return (
-    <div className="badge">
-      <img alt={name} src={img} />
+    <div style={style}>
+      <Avatar name={user.name} 
+      imgUrl={user.img} />
       <div>
-        <h4>{name}</h4>
-        <p>{handle}</p>
+        <Name name={user.name} />
+        <Handle handle={user.handle} />
+        <button onClick={addFriend}>Add Friend</button>
       </div>
     </div>
   );
 }
 
+
+
 export default function App() {
   return (
     <Badge
-      name="Tom Jerry"
-      handle="What time is it now?"
-      img = "https://halo-gemini.oss-cn-hangzhou.aliyuncs.com/blog/time-watch.jpg"
+      user={{
+        name: "Lynn Fisher",
+        img: "https://avatars.githubusercontent.com/u/871315",
+        handle: "hello world"
+      }}
+      style={{
+        width: 300,
+        margin: "0 auto",
+        border: "1px solid var(--beige-10)",
+        borderRadius: 8,
+        backgroundColor: "grey",
+        padding: 24,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "16px",
+        textAlign: "center"
+      }}
+      addFriend={() => alert("Added!")}
     />
   );
 }
