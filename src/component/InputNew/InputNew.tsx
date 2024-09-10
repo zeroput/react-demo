d9dda333bd3de602b725b95991436dc0cda25eb6
XiaShuang <zachary_xia@163.com>
2024-09-10 22:10:11 +0800

    refactor the input component, by passing the prop charLimit; add the css for the input label

--- src/component/InputNew/InputNew.tsx
+++ src/component/InputNew/InputNew.tsx
@@ -1,17 +1,24 @@
 import './InputNew.css'
 
-function InputNew() {
-    // need to use event.target.value to get the value of the input label
-    const myHandle = (event: any) => {
-        if (event.target.value.length > 5) {
-            alert('Your input has exceeded the limit')
+const myHandleOuter = (event: any, charLimit) => {
+    if (event.target.value.length > charLimit) {
+        alert(`Your input has exceeded the limit: ${charLimit}`)
+    }
+}
+
+function InputNew({ charLimit }) {
+    // inner handler in parent scope
+    const innerHandler = (event) => {
+        // need to use event.target.value to get the value of the input label
+        if (event.target.value.length > charLimit) {
+            alert(`Your input has exceeded the limit: ${charLimit}`)
         }
     }
     return (
         <>
             <section>
                 <h1>Character Limit 6 char</h1>
-                <input onChange={myHandle} placeholder="Enter some text" />
+                <input onChange={innerHandler} placeholder="Enter some text" />
             </section>
         </>
     )
@@ -19,6 +26,6 @@ function InputNew() {
 // why do I need to return the App() every time ? is this necessary?
 export default function App() {
     return (
-        <InputNew />
+        <InputNew charLimit={11} />
     )
 }
